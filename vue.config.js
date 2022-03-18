820c0681ef5dabb3905b412809f698ed2cef451f
Dan Gautsch <dgautsch@users.noreply.github.com>
2022-03-18 15:14:08 -0700

    Add main page title

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,10 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      title: 'Vow of the Disciple: Flashcards',
+    },
+  },
 });
